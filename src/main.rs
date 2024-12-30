741ea82a083659df5d780dbdebe619d05ba54072
scx1332 <sieciech.czajka@golem.network>
2024-12-30 18:36:13 +0100

    Working on server addressology

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,77 @@ mod hash;
 mod db;
 mod types;
 mod fancy;
+mod cookie;
 
-use std::path::PathBuf;
-use std::str::FromStr;
+
 use crate::hash::compute_create3_command;
-use actix_multipart::MultipartError;
-use actix_web::HttpRequest;
-use clap::{Parser, Subcommand};
 use crate::db::connection::create_sqlite_connection;
 use crate::db::ops::insert_fancy_obj;
+use actix_multipart::form::MultipartFormConfig;
+use actix_session::config::CookieContentSecurity;
+use actix_session::storage::CookieSessionStore;
+use actix_session::{Session, SessionMiddleware};
+use actix_web::cookie::SameSite;
+use actix_web::{
+    web, App, HttpRequest, HttpResponse, HttpResponseBuilder, HttpServer, Responder, Scope,
+};
+use awc::http::StatusCode;
+use awc::Client;
+use clap::{crate_version, Parser, Subcommand};
+use sqlx::SqlitePool;
+use std::collections::{BTreeSet, HashMap};
+use std::env;
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::sync::Arc;
+use lazy_static::lazy_static;
+use serde_json::json;
+use tokio::sync::Mutex;
+use crate::cookie::load_key_or_create;
+
+fn get_allowed_emails() -> Vec<String> {
+    let res = env::var("ALLOWED_EMAILS")
+        .unwrap_or("sieciech.czajka@golem.network".to_string())
+        .split(",")
+        .map(|x| x.trim().to_string())
+        .collect();
+    log::info!("Allowed emails loaded: {:?}", res);
+    res
+}
+
+fn get_domain() -> String {
+    let res = env::var("WEB_PORTAL_DOMAIN").unwrap_or("localhost".to_string());
+
+    log::info!("Portal domain: {}", res);
+
+    res
+}
+
+lazy_static! {
+    pub static ref ALLOWED_EMAILS: Vec<String> = get_allowed_emails();
+    pub static ref WEB_PORTAL_DOMAIN: String = get_domain();
+    static ref PASS_SALT: String = env::var("PASS_SALT").unwrap_or("LykwVQJAcU".to_string());
+    pub static ref ALLOW_CREATING_NEW_ACCOUNTS: bool = env::var("ALLOW_CREATING_NEW_ACCOUNTS")
+        .map(|v| v == "true")
+        .unwrap_or(false);
+}
+
+
+pub struct ServerData {
+    pub db_connection: Arc<Mutex<SqlitePool>>,
+}
+
+pub async fn handle_greet(session: Session) -> impl Responder {
+    println!("Session: {:?}", session.status());
+    let describe_version = crate_version!();
+
+    HttpResponse::Ok().json(json!({
+        "message": "Hello, World!",
+        "domain": *WEB_PORTAL_DOMAIN.clone(),
+        "version": describe_version,
+    }))
+}
+
 
 /// Enum that defines the available subcommands
 #[derive(Subcommand)]
@@ -31,7 +93,14 @@ enum Commands {
         #[arg(short, long)]
         miner: String,
     },
+    /// Start web server
+    Server {
+        #[arg(long, default_value = "localhost:80")]
+        addr: String,
 
+        #[arg(long)]
+        threads: Option<usize>,
+    },
 }
 
 #[derive(Parser)]
@@ -55,8 +124,46 @@ async fn main() -> std::io::Result<()> {
 
     let args = Cli::parse();
 
+    let secret_key = load_key_or_create("web-portal-cookie.key");
 
     match args.cmd {
+        Commands::Server { addr, threads } => {
+            let conn = create_sqlite_connection(Some(&PathBuf::from(args.db)), None, false, true)
+                .await
+                .unwrap();
+
+
+            HttpServer::new(move || {
+                let cors = actix_cors::Cors::permissive();
+
+                let server_data = web::Data::new(Box::new(ServerData {
+                    db_connection: Arc::new(Mutex::new(conn.clone())),
+                }));
+                let client = web::Data::new(Client::new());
+                let session_middleware =
+                    SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
+                        .cookie_secure(true)
+                        .cookie_content_security(CookieContentSecurity::Private)
+                        .cookie_same_site(SameSite::Strict)
+                        .cookie_domain(Some(WEB_PORTAL_DOMAIN.to_string()))
+                        .cookie_name("web-portal-session".to_string())
+                        .build();
+
+                let api_scope = Scope::new("/api")
+                    .route("/greet", web::get().to(handle_greet));
+
+                App::new()
+                    .wrap(session_middleware)
+                    .wrap(cors)
+                    .app_data(server_data)
+                    .app_data(client)
+                    .service(api_scope)
+            })
+                .workers(threads.unwrap_or(std::thread::available_parallelism().unwrap().into()))
+                .bind(addr)?
+                .run()
+                .await
+        }
         Commands::ComputeCreate3 {
             factory,
             salt,
