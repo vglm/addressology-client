7ea2ca6ca36354e081eba57df284ead6aa8434ce
scx1332 <sieciech.czajka@golem.network>
2025-01-04 22:51:35 +0100

    Working on editor

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 
 import { Routes, Route, Link, useNavigate, useLocation } from "react-router-dom";
-import LoginScreen from "./LoginScreen";
-//import { LoginStatus, useLoginEvent, useLoginOrNull } from "./LoginProvider";
-import { Avatar, Button, Menu, MenuItem } from "@mui/material";
-import { backendFetch } from "./common/BackendCall";
-import Blocks from "./Blocks";
-import ChangePassScreen from "./ChangePassScreen";
-import Transactions from "./Transactions";
-import Aggregates from "./Aggregates";
-import Aliases from "./Aliases";
-import Summary from "./Summary";
-import Charts from "./Charts";
-import DailyCharts from "./DailyCharts";
+import Editor from "react-simple-code-editor";
+//@ts-ignore
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-solidity';
+import 'prismjs/themes/prism.css'; //Example style, you can use another
+
 
 const Dashboard = () => {
     //const loginInformation = useLoginOrNull();
     const navigate = useNavigate();
+    const [code, setCode] = useState("// SPDX-License-Identifier: UNLICENSED\n" +
+        "pragma solidity ^0.8.28;\n" +
+        "\n" +
+        "// Uncomment this line to use console.log\n" +
+        "// import \"hardhat/console.sol\";\n" +
+        "\n" +
+        "contract Lock {\n" +
+        "    uint public unlockTime;\n" +
+        "    address payable public owner;\n" +
+        "\n" +
+        "    event Withdrawal(uint amount, uint when);\n" +
+        "\n" +
+        "    constructor(uint _unlockTime) payable {\n" +
+        "        require(\n" +
+        "            block.timestamp < _unlockTime,\n" +
+        "            \"Unlock time should be in the future\"\n" +
+        "        );\n" +
+        "\n" +
+        "        unlockTime = _unlockTime;\n" +
+        "        owner = payable(msg.sender);\n" +
+        "    }\n" +
+        "\n" +
+        "    function withdraw() public {\n" +
+        "        // Uncomment this line, and the import of \"hardhat/console.sol\", to print a log in your terminal\n" +
+        "        // console.log(\"Unlock time is %o and block timestamp is %o\", unlockTime, block.timestamp);\n" +
+        "\n" +
+        "        require(block.timestamp >= unlockTime, \"You can't withdraw yet\");\n" +
+        "        require(msg.sender == owner, \"You aren't the owner\");\n" +
+        "\n" +
+        "        emit Withdrawal(address(this).balance, block.timestamp);\n" +
+        "\n" +
+        "        owner.transfer(address(this).balance);\n" +
+        "    }\n" +
+        "}\n" +
+        "\n" );
 
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const urlParams = new URLSearchParams(window.location.search);
@@ -61,7 +91,25 @@ const Dashboard = () => {
 
     return (
         <div className="main-page" style={{ marginLeft: marginLeft }}>
-            Main page content
+            <div style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+            }}>
+                <Editor
+                    value={code}
+                    onValueChange={(newCode) => setCode(newCode)}
+                    padding={10}
+                    tabSize={4}
+                    highlight={code => highlight(code, languages.solidity)}
+                    style={{
+                        backgroundColor: "#f5f5f5",
+                        border: "1px solid #ddd",
+                        borderRadius: "5px",
+                        boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+                        fontSize: "14px",
+                        lineHeight: "20px",
+                    }}
+                />
+            </div>
         </div>
     );
 };
--- frontend/src/Summary.tsx
+++ frontend/src/Summary.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 
 import { backendFetch } from "./common/BackendCall";
 import { Aggregate, AggregateSummary, analyze_aggregates, analyze_summaries } from "./logic/Accounting";
@@ -11,6 +11,8 @@ import { getYearFromLocalStorageOrCurrent, useScans } from "./providers/CommonPr
 
 import { useNavigate } from "react-router-dom";
 
+import Editor from 'react-simple-code-editor';
+
 const getFirstDayInMonth = (year: number, month: number) => {
     return new Date(Date.UTC(year, month - 1, 1));
 };
@@ -19,89 +21,26 @@ const getLastDayInMonth = (year: number, month: number) => {
     return new Date(firstDay.setUTCMonth(firstDay.getUTCMonth() + 1));
 };
 
+
 const Summary = () => {
-    const [year, setYearInt] = React.useState(getYearFromLocalStorageOrCurrent());
-    const setYear = (year: number) => {
-        localStorage.setItem("year", year.toString());
-        setYearInt(year);
-    };
-    const navigate = useNavigate();
-    const firstDayInYear = getFirstDayInMonth(year, 1);
-    const lastDayInYear = getLastDayInMonth(year, 12);
-    lastDayInYear.setTime(lastDayInYear.getTime() - 1);
-
-    const [aliases, setAliases] = React.useState<Alias[]>([]);
-    const [currency, setCurrency] = React.useState("ETH");
-    const [blocks, setBlocks] = React.useState<Map<string, Array<Aggregate>>>(new Map());
-    const [loading, setLoading] = React.useState(false);
-    const scanInfo = useScans();
-
-    const getAliases = async () => {
-        const response = await backendFetch("/api/scan/aliases", {
-            method: "Get",
-        });
-        const aliases = await response.json();
-        setAliases(aliases);
-    };
-    const getAggregates = async () => {
-        setBlocks(new Map());
-        const blocksMap = new Map();
-        for (const scan of scanInfo.scans) {
-            const address = scan.address;
-            const url = `/api/scan/${address}/aggregates`;
-            const response = await backendFetch(url, {
-                method: "Get",
-            });
-            const data = await response.json();
-            const blocks = data["aggregates"];
-            blocksMap.set(address, blocks);
-        }
-        setBlocks(blocksMap);
-        setLoading(false);
-    };
-
-    useEffect(() => {
-        getAliases().then();
-    }, []);
-
-    useEffect(() => {
-        console.log("Scans changed - updating {}", scanInfo.scans.length);
-        if (scanInfo.scans.length == 0) {
-            return;
-        }
-        getAggregates().then();
-    }, [scanInfo.scans]);
-
-    const summaries = new Map();
-    for (const blocks1 of blocks.values()) {
-        const addr = blocks1[0]?.address ?? "";
-        const aggregates_result = analyze_aggregates(addr, blocks1, year);
-        const summary = aggregates_result.summary;
-        //const entries = aggregates_result.aggregates;
-        summaries.set(summary.address, summary);
-    }
-
-    const valuesUnsorted = Array.from(summaries.values());
-    const values = valuesUnsorted.sort((a, b) => {
-        return a.address.localeCompare(b.address);
-    });
-    const total = analyze_summaries(values);
-
-    function rowClass(aggr: AggregateSummary): string {
-        if (aggr.totalCheckZero != BigInt(0)) {
-            return "unbalanced";
-        }
-        return "check-ok-unfinished";
-    }
-
-
-    const renderAggregateEntry = (idx: number, block: AggregateSummary) => {
-
-    };
+    const [code, setCode] = useState("// Write your code here...\nconsole.log('Hello, World!');");
+    // Function to highlight code using Prism.js
 
     return (
         <div className={"content-main"}>
-            Summary
+            <Editor
+                value={code}
+                onValueChange={(newCode) => setCode(newCode)}
+                padding={10}
+                style={{
+                    backgroundColor: "#f5f5f5",
+                    border: "1px solid #ddd",
+                    borderRadius: "5px",
+                    boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+                    fontSize: "14px",
+                    lineHeight: "20px",
+                }}
+            />
         </div>
     );
 };
