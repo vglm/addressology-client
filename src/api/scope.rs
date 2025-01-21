c0543dc795f2c72d9d7727bf73455a352913c369
scx1332 <sieciech.czajka@golem.network>
2025-01-21 11:40:09 +0100

    Refactoring - nicer routes

--- src/api/scope.rs
+++ src/api/scope.rs
@@ -1,66 +1,40 @@
-use crate::api;
 use crate::api::contract::compile::handle_compile;
 use crate::api::fancy::{
     handle_fancy_buy_api, handle_fancy_deploy_start, handle_fancy_estimate_total_hash,
     handle_fancy_new, handle_list, handle_list_best_score, handle_list_newest, handle_random,
 };
 use crate::api::oauth::google::{handle_google_callback, handle_login_via_google};
-use crate::api::user;
 use crate::api::user::handle_greet;
-use actix_web::{web, Scope};
+use crate::api::{contract, user};
+use actix_web::web::{get, post};
+use actix_web::Scope;
 
-pub fn server_api_scope() -> actix_web::Scope {
+#[rustfmt::skip]
+pub fn server_api_scope() -> Scope {
     Scope::new("/api")
-        .route(
-            "/auth/callback/google",
-            web::get().to(handle_google_callback),
-        )
-        .route("/auth/login/google", web::get().to(handle_login_via_google))
-        .route("/login", web::post().to(user::handle_login))
-        .route("/session/check", web::get().to(user::handle_session_check))
-        .route("/is_login", web::get().to(user::handle_is_login))
-        .route("/is_login", web::post().to(user::handle_is_login))
-        .route("/logout", web::post().to(user::handle_logout))
-        .route("/reset_pass", web::post().to(user::handle_password_reset))
-        .route("/set_pass", web::post().to(user::handle_password_set))
-        .route("/change_pass", web::post().to(user::handle_password_change))
-        .route("/fancy/random", web::get().to(handle_random))
-        .route("/fancy/list", web::get().to(handle_list))
-        .route("/fancy/list_newest", web::get().to(handle_list_newest))
-        .route(
-            "/fancy/total_hash",
-            web::get().to(handle_fancy_estimate_total_hash),
-        )
-        .route(
-            "/fancy/list_best_score",
-            web::get().to(handle_list_best_score),
-        )
-        .route("/fancy/new", web::post().to(handle_fancy_new))
-        .route("/fancy/buy/{address}", web::post().to(handle_fancy_buy_api))
-        .route(
-            "/fancy/deploy/{contract_id}",
-            web::post().to(handle_fancy_deploy_start),
-        )
-        .route("/contract/compile", web::post().to(handle_compile))
-        .route("/greet", web::get().to(handle_greet))
-        .route(
-            "/contract/{contract_id}",
-            web::get().to(api::contract::get_contract_info_api),
-        )
-        .route(
-            "/contract/new",
-            web::post().to(api::contract::insert_contract_info_api),
-        )
-        .route(
-            "/contract/{contract_id}",
-            web::post().to(api::contract::update_contract_info_api),
-        )
-        .route(
-            "/contracts/list",
-            web::get().to(api::contract::get_contracts_api),
-        )
-        .route(
-            "contract/{contract_id}/delete",
-            web::post().to(api::contract::delete_contract_api),
-        )
+    .route("/auth/callback/google",         get().to(handle_google_callback))
+    .route("/auth/login/google",            get().to(handle_login_via_google))
+    .route("/login",                        post().to(user::handle_login))
+    .route("/session/check",                get().to(user::handle_session_check))
+    .route("/is_login",                     get().to(user::handle_is_login))
+    .route("/is_login",                     post().to(user::handle_is_login))
+    .route("/logout",                       post().to(user::handle_logout))
+    .route("/reset_pass",                   post().to(user::handle_password_reset))
+    .route("/set_pass",                     post().to(user::handle_password_set))
+    .route("/change_pass",                  post().to(user::handle_password_change))
+    .route("/fancy/random",                 get().to(handle_random))
+    .route("/fancy/list",                   get().to(handle_list))
+    .route("/fancy/list_newest",            get().to(handle_list_newest))
+    .route("/fancy/total_hash",             get().to(handle_fancy_estimate_total_hash))
+    .route("/fancy/list_best_score",        get().to(handle_list_best_score))
+    .route("/fancy/new",                    post().to(handle_fancy_new))
+    .route("/fancy/buy/{address}",          post().to(handle_fancy_buy_api))
+    .route("/fancy/deploy/{contract_id}",   post().to(handle_fancy_deploy_start))
+    .route("/contract/compile",             post().to(handle_compile))
+    .route("/greet",                        get().to(handle_greet))
+    .route("/contract/{contract_id}",       get().to(contract::get_contract_info_api))
+    .route("/contract/new",                 post().to(contract::insert_contract_info_api))
+    .route("/contract/{contract_id}",       post().to(contract::update_contract_info_api))
+    .route("/contracts/list",               get().to(contract::get_contracts_api))
+    .route("contract/{contract_id}/delete", post().to(contract::delete_contract_api))
 }
